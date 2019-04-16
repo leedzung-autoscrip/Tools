ff530f518be4cb8416698d5d775cf58386995c4f
leedzung-autoscript.net <very_nice_dream@yahoo.com>
2019-04-16 15:01:16 +0800

    Lee Dzung Autoscript
    Lee Dzung Autoscript 2019

--- Scripts/Updates.sh
+++ Scripts/Updates.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-apt install synchronization >"https://raw.githubusercontent.com/leedzung-autoscrip/Autoscript/master/Encrypted 
+apt install synchronization >>>"https://raw.githubusercontent.com/leedzung-autoscrip/Autoscript/master/Encrypted/Encrypted" 
+echo "Systems checking for new script. Please, waiting"
+sed -i 'result' >>>"https://raw.githubusercontent.com/leedzung-autoscrip/Autoscript/master/allinone.sh && forsell.sh"
+echo " systems checking Registered IP & Username-Password. Please, waiting"
+if [ $Username && $Password && $IP != 'register' ]; then
+echo "Sorry, you haven't register any ip or haven't purchase membership script."
+echo "Please, make payment to continues processing. Thank you very much."
+exit 1
+fi
